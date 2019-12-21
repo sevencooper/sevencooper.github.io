fda5319afba5f76a6deb60e1cc454773e801076b
sevencooper <sevencooper3412@gmail.com>
2019-12-20 23:30:07 -0500

    finished?

--- A9/app/js/main.js
+++ A9/app/js/main.js
@@ -12,69 +12,91 @@
  *    copied or excerpted for educational purposes with credit to the author.
  */
 var ScrabbleTiles = [];
-ScrabbleTiles["A"] = { "value": 1, "original-distribution": 9, "number-remaining": 9 };
-ScrabbleTiles["B"] = { "value": 3, "original-distribution": 2, "number-remaining": 2 };
-ScrabbleTiles["C"] = { "value": 3, "original-distribution": 2, "number-remaining": 2 };
-ScrabbleTiles["D"] = { "value": 2, "original-distribution": 4, "number-remaining": 4 };
-ScrabbleTiles["E"] = { "value": 1, "original-distribution": 12, "number-remaining": 12 };
-ScrabbleTiles["F"] = { "value": 4, "original-distribution": 2, "number-remaining": 2 };
-ScrabbleTiles["G"] = { "value": 2, "original-distribution": 3, "number-remaining": 3 };
-ScrabbleTiles["H"] = { "value": 4, "original-distribution": 2, "number-remaining": 2 };
-ScrabbleTiles["I"] = { "value": 1, "original-distribution": 9, "number-remaining": 9 };
-ScrabbleTiles["J"] = { "value": 8, "original-distribution": 1, "number-remaining": 1 };
-ScrabbleTiles["K"] = { "value": 5, "original-distribution": 1, "number-remaining": 1 };
-ScrabbleTiles["L"] = { "value": 1, "original-distribution": 4, "number-remaining": 4 };
-ScrabbleTiles["M"] = { "value": 3, "original-distribution": 2, "number-remaining": 2 };
-ScrabbleTiles["N"] = { "value": 1, "original-distribution": 6, "number-remaining": 6 };
-ScrabbleTiles["O"] = { "value": 1, "original-distribution": 8, "number-remaining": 8 };
-ScrabbleTiles["P"] = { "value": 3, "original-distribution": 2, "number-remaining": 2 };
-ScrabbleTiles["Q"] = { "value": 10, "original-distribution": 1, "number-remaining": 1 };
-ScrabbleTiles["R"] = { "value": 1, "original-distribution": 6, "number-remaining": 6 };
-ScrabbleTiles["S"] = { "value": 1, "original-distribution": 4, "number-remaining": 4 };
-ScrabbleTiles["T"] = { "value": 1, "original-distribution": 6, "number-remaining": 6 };
-ScrabbleTiles["U"] = { "value": 1, "original-distribution": 4, "number-remaining": 4 };
-ScrabbleTiles["V"] = { "value": 4, "original-distribution": 2, "number-remaining": 2 };
-ScrabbleTiles["W"] = { "value": 4, "original-distribution": 2, "number-remaining": 2 };
-ScrabbleTiles["X"] = { "value": 8, "original-distribution": 1, "number-remaining": 1 };
-ScrabbleTiles["Y"] = { "value": 4, "original-distribution": 2, "number-remaining": 2 };
-ScrabbleTiles["Z"] = { "value": 10, "original-distribution": 1, "number-remaining": 1 };
-ScrabbleTiles["_"] = { "value": 0, "original-distribution": 2, "number-remaining": 2 };
+ScrabbleTiles["A"] = { "value": 1, "distribution": 9 };
+ScrabbleTiles["B"] = { "value": 3, "distribution": 2 };
+ScrabbleTiles["C"] = { "value": 3, "distribution": 2 };
+ScrabbleTiles["D"] = { "value": 2, "distribution": 4 };
+ScrabbleTiles["E"] = { "value": 1, "distribution": 12 };
+ScrabbleTiles["F"] = { "value": 4, "distribution": 2 };
+ScrabbleTiles["G"] = { "value": 2, "distribution": 3 };
+ScrabbleTiles["H"] = { "value": 4, "distribution": 2 };
+ScrabbleTiles["I"] = { "value": 1, "distribution": 9 };
+ScrabbleTiles["J"] = { "value": 8, "distribution": 1 };
+ScrabbleTiles["K"] = { "value": 5, "distribution": 1 };
+ScrabbleTiles["L"] = { "value": 1, "distribution": 4 };
+ScrabbleTiles["M"] = { "value": 3, "distribution": 2 };
+ScrabbleTiles["N"] = { "value": 1, "distribution": 6 };
+ScrabbleTiles["O"] = { "value": 1, "distribution": 8 };
+ScrabbleTiles["P"] = { "value": 3, "distribution": 2 };
+ScrabbleTiles["Q"] = { "value": 10, "distribution": 1 };
+ScrabbleTiles["R"] = { "value": 1, "distribution": 6 };
+ScrabbleTiles["S"] = { "value": 1, "distribution": 4 };
+ScrabbleTiles["T"] = { "value": 1, "distribution": 6 };
+ScrabbleTiles["U"] = { "value": 1, "distribution": 4 };
+ScrabbleTiles["V"] = { "value": 4, "distribution": 2 };
+ScrabbleTiles["W"] = { "value": 4, "distribution": 2 };
+ScrabbleTiles["X"] = { "value": 8, "distribution": 1 };
+ScrabbleTiles["Y"] = { "value": 4, "distribution": 2 };
+ScrabbleTiles["Z"] = { "value": 10, "distribution": 1 };
+ScrabbleTiles["_"] = { "value": 0, "distribution": 2 };
 
-// Tile array
-var tiles;
+// Array of remaining tiles
+var remainingTiles = [];
+
+for (var key in ScrabbleTiles) {
+    for (i = 0; i < ScrabbleTiles[key].distribution; ++i) {
+        remainingTiles.push(key);
+    }
+}
 
 // Track the score across multiple words
 var totalScore = 0;
 // Track the score of the current word
 var letterScore = 0;
 var numDoubleWord = 0;
-var numDoubleLetter = 0;
 
-// Map of the collided tiles
-const collidedTiles = new Object();
+// Dictionary of the collided tiles
+var collidedTiles = {};
 
 // Number of tiles to generate at once
-const NUM_TILES = 15;
+const NUM_TILES = 7;
 
 
 $().ready(function () {
     generateTiles();
+
+    $("#button-submit").click(function (event) {
+        totalScore += letterScore * Math.pow(2, numDoubleWord);
+        generateTiles();
+    });
 });
 
 function generateTiles() {
+    // Reset variables
+    letterScore = 0;
+    numDoubleWord = 0;
+    collidedTiles = {};
+
     // Remove the old tiles
     $("#tiles").empty();
 
-    // Clear tiles array
-    tiles = [];
-
     for (i = 0; i < NUM_TILES; ++i) {
-        tiles[i] = generateTile(i, 'Q');
-        $("#" + tiles[i]).css({
-            'top': $("#tile-holder").position().top - ($("#" + tiles[i]).height() * i) + 5,
-            'left': $("#tile-holder").position().left + (74 * i) // 72 is the width of a tile
+        if (remainingTiles.length === 0) {
+            break;
+        }
+
+        // Generate a random index
+        var index = Math.floor(Math.random() * remainingTiles.length);
+        var letter = remainingTiles[index];
+        // Remove the letter
+        remainingTiles.splice(index, 1);
+
+        var id = generateTile(i, letter);
+        $("#" + id).css({
+            'top': $("#tile-holder").position().top - ($("#" + id).height() * i) + 5,
+            'left': $("#tile-holder").position().left + (80 * i) + 300 // 72 is the width of a tile
         });
-        $("#" + tiles[i]).draggable({
+        $("#" + id).draggable({
             snap: ".snaptarget",
             stop: onTileStopped
         });
@@ -97,32 +119,32 @@ function onTileStopped(event, ui) {
     var position = tile.position();
 
     var collisionClass = checkForCollision(position);
+    var prevCollision = collidedTiles[event.target.id];
+    var tileValue = ScrabbleTiles[key].value;
 
+    // Clear old score from this tile
+    if (prevCollision === "default") {
+        letterScore -= tileValue;
+    } else if (prevCollision === "double-letter") {
+        letterScore -= 2 * tileValue;
+    } else if (prevCollision === "double-word") {
+        letterScore -= tileValue;
+        numDoubleWord--;
+    }
+
+    // Re-calculate score for the tile's new position
     if (collisionClass === "default") {
-        if (collidedTiles.has(event.target.id)) {*
-            // score -= ...
-        } else {
-            collidedTiles.set(event.target.id, collisionClass);
-            // score += ...
-        }
+        collidedTiles[event.target.id] = collisionClass;
+        letterScore += tileValue;
     } else if (collisionClass === "double-letter") {
-        if (collidedTiles.has(event.target.id)) {
-            // score -= ...
-        } else {
-            collidedTiles.set(event.target.id, collisionClass);
-            // score += ...
-        }
+        collidedTiles[event.target.id] = collisionClass;
+        letterScore += 2 * tileValue;
     } else if (collisionClass === "double-word") {
-        if (collidedTiles.has(event.target.id)) {
-            // score -= ...
-        } else {
-            collidedTiles.set(event.target.id, collisionClass);
-            // score += ...
-        }
+        collidedTiles[event.target.id] = collisionClass;
+        letterScore += tileValue;
+        numDoubleWord++;
     } else {
-        if (collidedTiles.has(event.target.id)) {
-            // score -= ...
-        }
+        delete collidedTiles[event.target.id];
     }
 
     updateScoreInDom();
@@ -143,10 +165,8 @@ function checkForCollision(position) {
     return returnVal;
 }
 
-function updateCurrentScoreValues() {
-
-}
-
 function updateScoreInDom() {
-
+    var calculatedScore = letterScore * Math.pow(2, numDoubleWord);
+    var combinedScore = parseInt(totalScore) + calculatedScore;
+    $("#score").html("Score: " + combinedScore);
 }
